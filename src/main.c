8f44d84c8a7d1b7e937889d362492632ec7e566e
edroque93 <quique.lpgc@gmail.com>
2017-09-19 11:37:33 +0200

    base problem

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
 // Enrique Díaz Roque
 // IngeniaMC Exercise 2
 
-// WC Time Complexity O(N)
-// WC Space Complexity O(1)
-int solution(int a[], int n) { return 0; }
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) { return 0; }
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+int solution(int a[], int n) {
+  // SC O(1)
+  int min_buy = INT_MAX;
+  int current_profit = INT_MIN;
+  int best_profit = 0;
+  // TC O(N)
+  for (int i = 0; i < n; i++) {
+    current_profit = MAX(0, a[i] - min_buy);
+    min_buy = MIN(a[i], min_buy);
+    best_profit = MAX(current_profit, best_profit);
+  }
+  return best_profit;
+}
+
+int main(int argc, char **argv) {
+  int a[6] = {23171, 21011, 21123, 21366, 21013, 21367};
+  printf("Solution: %d\n", solution(a, 6));
+
+  return 0;
+}
